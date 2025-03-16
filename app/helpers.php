abe8ff7f280245782a479acae672a5f5d37c8a3b
ipkpjersi <ipkpjersi@gmail.com>
2025-03-16 17:38:03 -0400

    Added some helper functions

--- app/helpers.php
+++ app/helpers.php
@@ -23,3 +23,47 @@ if (! function_exists('get_client_ip_address')) {
         return $clientIp;
     }
 }
+
+if (! function_exists('rot19')) {
+    function rot19($string)
+    {
+        $result = '';
+        foreach (str_split($string) as $char) {
+            $ascii = ord($char);
+            if ($ascii >= ord('a') && $ascii <= ord('z')) {
+                $result .= chr((($ascii - ord('a') + 19) % 26) + ord('a'));
+            } elseif ($ascii >= ord('A') && $ascii <= ord('Z')) {
+                $result .= chr((($ascii - ord('A') + 19) % 26) + ord('A'));
+            } else {
+                $result .= $char;
+            }
+        }
+
+        return $result;
+    }
+}
+
+if (! function_exists('safe_json_encode')) {
+    function safe_json_encode($data)
+    {
+        if (empty($data) || in_array($data, ['[]', '{}', 'null', 'NULL', ''])) {
+            return null;
+        }
+        $encodedData = json_encode($data);
+        if (in_array($encodedData, ['[]', '{}', '""', 'null', 'NULL'])) {
+            return null;
+        }
+
+        return $encodedData;
+    }
+}
+
+if (! function_exists('getBaseUrl')) {
+    function getBaseUrl() {
+        $scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+        $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+        $port = $_SERVER['SERVER_PORT'] ?? null;
+
+        return $port && !in_array($port, [80, 443]) ? "$scheme://$host:$port" : "$scheme://$host";
+    }
+}
